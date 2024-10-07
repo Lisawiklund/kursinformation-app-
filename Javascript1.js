f8a538edf85132ada98b549fadd0a4eca2f2de03
Lisawiklund <119859496+Lisawiklund@users.noreply.github.com>
2024-10-07 19:19:27 +0200

    Add files via upload

--- Javascript1.js
+++ Javascript1.js
@@ -1,46 +1,20 @@
 //Hämtar data till console.log
 fetch("https://webbred2.utb.hb.se/~fewe/api/api.php?data=quiz")
 .then(res => res.json())
-.then(data => console.log(data))
+.then(data => {
+    console.log("Quiz data:", data);  // Kontrollera att datan faktiskt hämtas
 
-//Hämtar data till console.log
-fetch("https://webbred2.utb.hb.se/~fewe/api/api.php?data=courses")
-.then(res => res.json())
-.then(data => console.log(data))
+    //Hämtar data i json format och skriver ut den i i form av en lista med hjälp av div element.
+    data.forEach(quiz => {
+        // Skapar en lista av datan för att visa
+        const lista = `<div>
+            <div><strong>Fråga:</strong> ${quiz.question}?</div> 
+            <div><strong>Svar:</strong> ${quiz.correct_answer} <br> 
+            <em>Felaktiga svar:</em> ${quiz.incorrect_answers.join(', ')}</div><br>
+        </div>`;
 
-//Courses
-//Hämtar data i json format och skriver ut den i en tabell.
-fetch("https://webbred2.utb.hb.se/~fewe/api/api.php?data=courses", {
-    method: "GET"
+        // Anger position om hur texten i listan ska visas, alltså varje stycke efter varandra.
+        document.querySelector("#output2").insertAdjacentHTML("beforeend", lista);
+    });
 })
-   .then(res => res.json())
-   .then(data => {
-     data.forEach(courses => {
-       const kurser = `<tr> 
-       <td>${courses.courseName}</td> 
-       <td> ${courses.credit}</td>
-        <td> ${courses.startWeek}</td> 
-        <td> ${courses.endWeek}</td>
-        <td> ${courses.teachers}</td>
-      </tr>`;
-       document.querySelector("#output > tbody").insertAdjacentHTML("beforeend", kurser)
-     })
-   }) //Om det blir något fel i koden visas detta meddelande i console.log "error".
-      .catch(error => console.log("Error: ", error));
-
-   //Quiz
-   //Hämtar data i json format och skriver ut den i i form av en lista med hjälp av div element.
-   fetch("https://webbred2.utb.hb.se/~fewe/api/api.php?data=quiz", {
-    method: "GET"
-})
-   .then(res => res.json())
-   .then(data => {
-     data.forEach(quiz => { //Skapar en lista av datan för att visa
-       const lista = `<div> 
-    <div> Fråga: <br>  ${quiz.question}? </div> <br>
-    <div> Svar: <br>  ${quiz.correct_answer }   ${quiz.incorrect_answers}</div> <br>
-                    </div>`; //Anger position om hur texten i listan ska visas, alltså varje stycke efter varandra.
-       document.querySelector("#output2 > div").insertAdjacentHTML("beforeend", lista)
-     })
-   })  //Om det blir något fel i koden visas detta meddelande i console.log "error".
-   .catch(error => console.log("Error: ", error));
+.catch(error => console.log("Error fetching quiz data:", error));
